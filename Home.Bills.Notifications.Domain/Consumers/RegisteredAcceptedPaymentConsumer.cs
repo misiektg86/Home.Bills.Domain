739388225d31099827bbb6b5bd168ab08cfdbc33
misiektg86 <misiektg86@gmail.com>
2017-01-23 22:33:48 +0100

    changes

---
+++ Home.Bills.Notifications.Domain/Consumers/RegisteredAcceptedPaymentConsumer.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Home.Bills.Notifications.Domain.AddressAggregate;
+using MassTransit;
+
+namespace Home.Bills.Notifications.Domain.Consumers
+{
+    public class RegisteredAcceptedPaymentConsumer : IConsumer<RegisteredAcceptedPayment>
+    {
+        public Task Consume(ConsumeContext<RegisteredAcceptedPayment> context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
